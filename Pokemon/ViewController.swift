9479b90ab760bac5f4eb7bdefe830e91974063b5
Mehmet Tarhan <hi@memtarhan.com>
2024-02-01 22:17:09 +0000

    <utils>: created project

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Mehmet Tarhan on 01/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
